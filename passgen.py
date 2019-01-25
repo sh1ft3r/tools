3bf6373cf00d2011e9f95765a449431f5ce76889
Kerim Sancakdar <32750348+sh1ft3r@users.noreply.github.com>
2019-01-25 17:05:02 +0100

    passgen.py
    This is a password generator I wrote that has a feature to have comments with the password so you can remember what password for what case is. 
    It also copies the generated password to clipboard.

---
+++ passgen.py
@@ -0,0 +1,22 @@
+############# version: v0.11 ##################
+############# Author: Kerim ##################
+
+#!/usr/bin/env python
+
+import string
+import os
+from random import *
+
+#creating a random password with letters,digits and punctiatons
+characters = string.ascii_letters + string.digits + string.punctuation
+password = "".join(choice(characters) for x in range (randint(8,12)))
+
+#input for a comment
+name = raw_input("Comment for what this password is for: ")
+
+
+print "This password is for: %s." % name
+print password
+
+# copy password to clipboard
+os.system("echo '%s' | pbcopy" % password)
